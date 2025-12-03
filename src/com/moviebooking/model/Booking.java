97ac73bbe8bf96f0a18b6ac3941c18bef578fcea
alihalilalmustafa-bit <alihalilalmustafa@gmail.com>
2025-12-03 18:27:04 +0300

    Created Booking class with price calculation logic

---
+++ src/com/moviebooking/model/Booking.java
@@ -0,0 +1,40 @@
+package com.moviebooking.model;
+
+/**
+ * Represents a final booking transaction.
+ * Links Customer, ShowTime, and Seat together.
+ */
+public class Booking {
+
+    private Customer customer;
+    private ShowTime showTime;
+    private Seat seat;
+    private double totalPrice;
+
+    public Booking(Customer customer, ShowTime showTime, Seat seat) {
+        this.customer = customer;
+        this.showTime = showTime;
+        this.seat = seat;
+        this.totalPrice = calculateTotal();
+    }
+
+    /**
+     * Calculates the price based on movie type (Polymorphism).
+     */
+    private double calculateTotal() {
+        return showTime.getMovie().calculatePrice();
+    }
+
+    public void confirmBooking() {
+        seat.book();
+        System.out.println("Booking confirmed for " + customer.getName());
+        System.out.println("Movie: " + showTime.getMovie().getTitle());
+        System.out.println("Seat: " + seat.getSeatId());
+        System.out.println("Price: $" + totalPrice);
+    }
+
+    // Getters
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+}
